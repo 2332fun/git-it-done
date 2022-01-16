f12199e78a711e2896f4a35f298348e7bdb49d51
DCT <mirachan2332@gmail.com>
2022-01-16 11:07:04 -0600

    fix

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -67,51 +67,6 @@ var displayRepos = function(repos, searchTerm) {
     repoContainerEl.textContent = "No repositories found.";
     return;
   }
-<<<<<<< HEAD
-    // clear old content
-  repoContainerEl.textContent = "";
-  repoSearchTerm.textContent = searchTerm;
-  console.log(repos);
-  console.log(searchTerm);
-  // loop over repos
-for (var i = 0; i < repos.length; i++) {
-  // format repo name
-  var repoName = repos[i].owner.login + "/" + repos[i].name;
-
-  // create a container for each repo
-  var repoEl = document.createElement("div");
-  repoEl.classList = "list-item flex-row justify-space-between align-center";
-
-  // create a span element to hold repository name
-  var titleEl = document.createElement("span");
-  titleEl.textContent = repoName;
-
-  // append to container
-  repoEl.appendChild(titleEl);
-
-  // create a status element
-  var statusEl = document.createElement("span");
-  statusEl.classList = "flex-row align-center";
-
-  // check if current repo has issues or not
-  if (repos[i].open_issues_count > 0) {
-    statusEl.innerHTML =
-      "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
-  } else {
-    statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-  }
-
-  // append to container
-  repoEl.appendChild(statusEl);
-  // append container to the dom
-  repoContainerEl.appendChild(repoEl);
-}
-};
-
-getUserRepos("lernantino");
-
-userFormEl.addEventListener("submit", formSubmitHandler);
-=======
 
   repoSearchTerm.textContent = searchTerm;
 
@@ -164,8 +119,4 @@ var buttonClickHandler = function(event) {
 
 // add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
-<<<<<<< HEAD
->>>>>>> develop
-=======
 languageButtonsEl.addEventListener("click", buttonClickHandler);
->>>>>>> develop
